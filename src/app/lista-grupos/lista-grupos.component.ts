6d124badac9ccbc678dbc04606971f3e60d69b8f
daniel silva <leinadart@gmail.com>
2020-11-03 19:48:51 -0500

    update services groups

--- src/app/lista-grupos/lista-grupos.component.ts
+++ src/app/lista-grupos/lista-grupos.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit, ViewChild, HostListener, AfterViewInit, ChangeDetectorRef } from '@angular/core';
+import { Component, OnInit, ViewChild,  AfterViewInit, ChangeDetectorRef } from '@angular/core';
 import {GrupoServiceService} from '../grupo-service.service';
 import { AngularFirestore} from '@angular/fire/firestore';
 import { MdbTablePaginationComponent, MdbTableDirective} from 'angular-bootstrap-md';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-lista-grupos',
@@ -12,10 +13,15 @@ export class ListaGruposComponent implements OnInit, AfterViewInit {
   @ViewChild(MdbTablePaginationComponent, { static: true }) mdbTablePagination: MdbTablePaginationComponent; //Se agregaron las lineas 14 y 15 para ejemplo de tabla
   @ViewChild(MdbTableDirective, { static: true }) mdbTable: MdbTableDirective;
   groupobject:any []=[];
+  locks:any []=[];
+  id:any={'id':''};
+  selectobject: string [];
+
+  selectid:string='';
   
   constructor(
-    private db: AngularFirestore,
     private cdRef: ChangeDetectorRef,
+    private router: Router,
     private group: GrupoServiceService
   ) { }
 
@@ -30,17 +36,29 @@ export class ListaGruposComponent implements OnInit, AfterViewInit {
     this.cdRef.detectChanges();
   }
 
+
+
   getGroupAll =()=>this.group.getGroupAll().subscribe(response =>{
     
     this.groupobject=[];
-    for (let order of response){
-          this.groupobject.push(order.payload.doc.data());
-    }
-    // console.log(this.groupobject);
+    this.selectobject=[];
+    this.id=[];
 
+    for (let order of response)         
+          this.groupobject.push(order.payload.doc.data());          
+
+    for (let i=0;i<this.groupobject.length;i++){
+        this.selectobject[i]=this.groupobject[i].nombre;
+        }  
     this.mdbTable.setDataSource(this.groupobject);//nuevo
     this.groupobject= this.mdbTable.getDataSource();
     //this.previous = this.mdbTable.getDataSource();
   });
+
+
+  gotoeditgroup(route:string){
+    this.router.navigate(["/grupos",route]);
+ 
+  }
 
 }
